15d6f047be2be952feaea6acb672d731e7f2ffa9
gazsoulriper <gazsoulriper@gmail.com>
2024-01-11 06:45:08 -0300

    adjusts

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = '';
+    const url = `https://github.com/gazsoulriper/js-developer-portfolio/blob/main/assets/data/profile.json`;
     const fetching = await fetch(url)
     return await fetching.json()
 }
