ef7ee34bb2a17153a6439c72caff8fb02fa5034c
gazsoulriper <gazsoulriper@gmail.com>
2024-01-11 07:53:43 -0300

    adjusts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+}
+
+
+
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfileInfo(profileData)
 })()
 
 
