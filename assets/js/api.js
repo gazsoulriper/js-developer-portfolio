1cc9830523bb60b6da3b047246e61b2ca48d723f
gazsoulriper <gazsoulriper@gmail.com>
2024-01-12 00:10:14 -0300

    alteração das importações dos assets

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = `https://raw.githubusercontent.com/gazsoulriper/js-developer-portfolio/main/assets/data/profile.json`;
+    const url = `https://raw.githubusercontent.com/gazsoulriper/js-developer-portfolio/mainassets/data/profile.json`;
     const fetching = await fetch(url)
     return await fetching.json()
 }
